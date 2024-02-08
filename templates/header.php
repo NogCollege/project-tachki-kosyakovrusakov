9ef3666c7103191cbdfb435fefda1cb6cec87565
Th3Nerwas <kolt12566@gmail.com>
2024-02-08 13:24:19 +0300

    добавление php файлов секции

---
+++ templates/header.php
@@ -0,0 +1,30 @@
+<header>
+        <div class="nav-menu">
+          <ul class="menu">
+            <div class="logo">
+              <li><h1>АРЕНДАТАЧЕК.РФ</h1></li>
+            </div>
+            <div class="knopki no-vid" >  
+              <li>Подобрать авто</li>
+              <li>Условия</li>
+              <li>Цены</li>
+              <li>О нас</li>
+              <li>Контакты</li>
+            </div>
+            <div class="media no-vid">
+              <li><img src="img/Vector (33).png" alt=""></li>
+              <li><img src="img/Vector (32).png" alt=""></li>
+            </div>
+            <div class="nomer no-vid">
+              <li>7 (499) 110-20-47</li>
+            </div>
+            <div class="button1 no-vid">
+              <li><button>Заказать звонок</button></li>
+            </div>
+            <div class="fotop">
+              <li><img src="img/Group 71 (1).png" alt=""></li>
+              <li><img src="img/Group 72 (1).png" alt=""></li>
+            </div>
+          </ul>
+        </div>
+      </header>
