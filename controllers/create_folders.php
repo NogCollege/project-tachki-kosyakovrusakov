e50d6145240b0475bec567d8cfd38e9c99f9b518
Th3Nerwas <kolt12566@gmail.com>
2024-02-16 10:36:49 +0300

    создали скрипт и начали добавлять фотки машин

--- controllers/create_folders.php
+++ controllers/create_folders.php
@@ -1,26 +1,29 @@
-<?php
-   $host = 'localhost'; 
-   $user = 'root'; 
-   $password = ''; 
-   $db_name = 'sait'; 
-   $link = mysqli_connect($host, $user, $password, $db_name);
-   
-   $sql = "SELECT id, name FROM catalogg";
-   $result = mysqli_query($link, $sql);
-   
-   if (mysqli_num_rows($result) > 0) {
-       while ($row = mysqli_fetch_assoc($result)) {
-           $folderName = $row['id'] . '-' . strtolower(str_replace(' ', '', $row['fullname']));
-           $folderPath = 'templates/img/photos/' . $folderName;
-   
-           if (!is_dir($folderPath)) {
-               mkdir($folderPath);
-               echo "Папка $folderName успешно создана<br>";
-           }
-       }
-   } else {
-       echo "Нет доступных автомобилей";
-   }
-   
-   mysqli_close($link);
+<?php 
+    require_once('connect.php');
+
+    if ($link->connect_error) {
+        die("Ошибка подключения к базе данных: " . $link->connect_error);
+    }
+
+    $sql = "SELECT id, fullname FROM catalogg";
+    $result = $link->query($sql);
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $id = $row["id"];
+            $name = $row["fullname"];
+            $folderName = $id . "-" . preg_replace("/[^A-Za-z0-9]/", "", $name);
+
+            $folderPath = "../templates/img/photos/" . $folderName;
+            if (!is_dir($folderPath)) {
+                mkdir($folderPath);
+                echo "Папка " . $folderName . " успешно создана.<br>";
+            }
+        }
+    } else {
+        echo "Нет данных об автомобилях в базе.";
+    }
+
+    $link->close();
+
 ?>
