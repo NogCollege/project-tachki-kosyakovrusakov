71db4fc343fd5d9b831e69489e15d68d27fbc591
Th3Nerwas <kolt12566@gmail.com>
2024-02-16 13:36:25 +0300

    начали делать Java script

--- templates/avtopark.php
+++ templates/avtopark.php
@@ -5,28 +5,73 @@
         </div>
         <div class="tri container">
             <ul class="catalog">
-                <li class="dark">
-                    <img src="templates\img/Group 47.png" alt="">
+                <li>
+                    <button class="button-cat dark" data-category="Внедорожники">
+                    <img src="templates\img/Group 47.png" alt=""> 
                     Внедорожники
+                    </button>
                 </li>
-                <li class="svet s1">
+                <li >
+                    <button class="button-cat svet s1" data-category="Бизнес">
                     <img src="templates\img/Group 47 (1).png" alt="">
                     Бизнес
+                    </button>
                 </li>
-                <li class="svet s2">
+                <li>
+                    <button class="button-cat svet s1" data-category="Спорткар">
                     <img src="templates\img/Group 48 (1).png" alt="">
                     Спорткар
+                    </button>
                 </li>
-                <li class="svet s3">
+                <li>
+                    <button class="button-cat svet s1" data-category="Премиум">
                     <img src="templates\img/Group 47 (2).png" alt="">
                    <p>Премиум</p>
+                   </button>
                 </li>
-                <li class="svet s4">
+                <li>
+                    <button class="button-cat svet s1" data-category="Комфорт">
                     <img src="templates\img/Group 47 (3).png" alt="">
                     <p>Комфорт</p>
+                    </button>
                 </li>
             </ul>
         </div>
+    <script>
+        let buttons = document.querySelectorAll('.button-cat')
+
+        let category = new Map([
+        ['Внедорожники', document.querySelectorAll('.cat-Внедорожники')],
+        ['Бизнес', document.querySelectorAll('.cat-Бизнес')],
+        ['Спорткар', document.querySelectorAll('.cat-Спорткар')],
+        ['Премиум', document.querySelectorAll('.cat-Премиум')],
+        ['Комфорт', document.querySelectorAll('.cat-Комфорт')]
+]); 
+        let allCards = document.querySelectorAll('.catalog-list-item')
+        function DisplayCards(cat) {
+            allCards.forEach((card) => {
+                card.style.display = 'none'
+})
+        category.get(cat).forEach((card) => {
+            card.style.display = 'block'
+})
+}
+        for (let button of buttons) {
+            button.addEventListener("click", function() {
+                if (!button.classList.contains("button-active")) {
+                    console.log("Нажатие на неактивную кнопку")
+                    buttons.forEach((button) => {
+                        button.classList.remove("button-active") // убираем класс у всех кнопок
+                        this.classList.add("button-active") // добавляем класс к нажатой кнопке
+                    })
+                DisplayCards(this.dataset.category)
+                } else {
+                console.log("Нажатие на активную кнопку")
+            }
+        })
+    }
+
+</script>
 <?php 
     require_once ("controllers/connect.php");
     
